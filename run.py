fe0284bac7cb423a49f02430ac470e2c49548836
amir <a_emad@knights.ucf.edu>
2018-02-13 10:47:09 -0500

    Revert "Indentation Fix"
    This reverts commit 2b50520fae7700715b28e26e919199a63a79f925.

--- run.py
+++ run.py
@@ -11,82 +11,82 @@ from utils.trainingutils.training_phases_utils import *
 
 
 def check_model_list(model_list, datasets):
-    opts = default_opt_creator()
-    for dataset_str in datasets:
-        for model in model_list:
-            set_dataset(opts, dataset=dataset_str)
-            set_default_opts_based_on_model_dataset(opts)
-            set_model_string(opts, model)
-            model_dict= get_model_from_db(model, opts)
-            model = model_dict['model']
-            model.summary()
+	opts = default_opt_creator()
+	for dataset_str in datasets:
+		for model in model_list:
+			set_dataset(opts, dataset=dataset_str)
+			set_default_opts_based_on_model_dataset(opts)
+			set_model_string(opts, model)
+			model_dict= get_model_from_db(model, opts)
+			model = model_dict['model']
+			model.summary()
 
 
 
 if __name__ == '__main__':
-    for total_params in [0,3.7,1,2]:
-        models = ['vgg_baseline']
-        datasets = ['cifar100','cifar10']
-        experiment_name = get_experiment_name_prompt()
-        check_model_list(models, datasets)
-        print((keras.__version__))
-        for dataset_str in datasets:
-            # set dataset_params
-            for model_str in models:
-                try:
+	# gatenet_binary_merged_model lenet_amir ,gatenet_binary_model
+	for total_params in [.5,3.7,1,2]:
+	# total_params=1;
+		models = ['vgg_baseline']
+		datasets = ['cifar100','cifar10']
+		experiment_name = get_experiment_name_prompt()
+		check_model_list(models, datasets)
+		print((keras.__version__))
+		for dataset_str in datasets:
+			# set dataset_params
+			for model_str in models:
+				try:
 
-                    print(100 * '*', 3 * '\n', model_str, '\n', dataset_str, 3 * '\n', 100 * '*')
-                    opts = default_opt_creator()
-                    opts['experiment_name'] = experiment_name
+					print(100 * '*', 3 * '\n', model_str, '\n', dataset_str, 3 * '\n', 100 * '*')
+					opts = default_opt_creator()
+					opts['experiment_name'] = experiment_name
 
-                    set_dataset(opts, dataset=dataset_str)
-                    opts = set_model_string(opts, model_str)
-                    opts = set_default_opts_based_on_model_dataset(opts)
+					set_dataset(opts, dataset=dataset_str)
+					opts = set_model_string(opts, model_str)
+					opts = set_default_opts_based_on_model_dataset(opts)
 
-                    input_shape = opts['training_opts']['dataset']['input_shape']
-                    nb_class = opts['training_opts']['dataset']['nb_classes']
-                    # opts = set_expand_rate(opts, param_expand_sel)
-                    # optimizer = optimizers.Nadam()
-                    optimizer = optimizers.SGD(lr=opts['optimizer_opts']['lr'], momentum=opts['optimizer_opts']['momentum'],
-                                               decay=opts['optimizer_opts']['decay'], nesterov=opts['optimizer_opts']['nestrov'])
-                    # optimizer = optimizers.Adadelta()
-                    """ MODEL PREPARE """
-                    model_dict = get_model_from_db(model_str, opts)
-                    model = model_dict['model']
-                    model_total_params = (model.count_params() // 100000) / 10
-                    if (not total_params == 0) and (not model_total_params== total_params):
-                        set_expand_rate(opts, np.sqrt(total_params/model_total_params)*get_expand_rate(opts));
-                        print('Expand Rate Changed')
-                        model_dict = get_model_from_db(model_str, opts)
-                        model = model_dict['model']
-                    if total_params==0: total_params = model_total_params;
+					input_shape = opts['training_opts']['dataset']['input_shape']
+					nb_class = opts['training_opts']['dataset']['nb_classes']
+					# opts = set_expand_rate(opts, param_expand_sel)
+					# optimizer = optimizers.Nadam()
+					optimizer = optimizers.SGD(lr=opts['optimizer_opts']['lr'], momentum=opts['optimizer_opts']['momentum'],
+					                           decay=opts['optimizer_opts']['decay'], nesterov=opts['optimizer_opts']['nestrov'])
+					# optimizer = optimizers.Adadelta()
+					""" MODEL PREPARE """
+					model_dict = get_model_from_db(model_str, opts)
+					model = model_dict['model']
+					model_total_params = (model.count_params() // 100000) / 10
+					if (not total_params == 0) and (not model_total_params== total_params):
+						set_expand_rate(opts, np.sqrt(total_params/model_total_params)*get_expand_rate(opts));
+						print('Expand Rate Changed')
+						model_dict = get_model_from_db(model_str, opts)
+						model = model_dict['model']
+					if total_params==0: total_params = model_total_params;
 
-                    opts['experiment_tag'] = experiment_name + '/' + dataset_str + '/' + model_str + '/' + str((model.count_params()//100000)/10)+'M'
-                    # out_tensor_list  = model_dict['out']
-                    # output_num  = len(out_tensor_list)
-                    model.summary()
-                    # model_modification_utils.load_weights_by_block_index_list(model, [1, 2, 3, 4, 5, 6, 7, 8, 9],
-                    #  os.path.join(
-                    # 	global_constant_var.get_experimentcase_abs_path(experiment_name, dataset_str, 'nin_tree_berp_1')
-                    # , 'checkpoint'),
-                    #                                                           model_constructor_utils.CONVSH_NAME)
-                    model.compile(loss=opt_utils.get_loss(opts), optimizer=optimizer, metrics=opt_utils.get_metrics(opts))
-                    method_names = find_key_value_to_str_recursive(opts, '', {'param_expand'})
-                    opts['experiment_name'] = method_names
-                    # LOAD DATA
-                    (data_train, label_train), (data_test, label_test) = load_data(dataset_str, opts)
-                    data_train, data_test = preprocess_data_phase(opts, data_train, data_test)
-                    data_gen = data_augmentation_phase(opts)
-                    # COLLECT CALLBACKS
-                    callback_list = collect_callbacks(opts)
+					opts['experiment_tag'] = experiment_name + '/' + dataset_str + '/' + model_str + '/' + str((model.count_params()//100000)/10)+'M'
+					# out_tensor_list  = model_dict['out']
+					# output_num  = len(out_tensor_list)
+					model.summary()
+					# model_modification_utils.load_weights_by_block_index_list(model, [1, 2, 3, 4, 5, 6, 7, 8, 9], os.path.join(
+					# 	global_constant_var.get_experimentcase_abs_path(experiment_name, dataset_str, 'nin_tree_berp_1'), 'checkpoint'),
+					#                                                           model_constructor_utils.CONVSH_NAME)
+					model.compile(loss=opt_utils.get_loss(opts), optimizer=optimizer, metrics=opt_utils.get_metrics(opts))
+					method_names = find_key_value_to_str_recursive(opts, '', {'param_expand'})
+					opts['experiment_name'] = method_names
+					# LOAD DATA
+					(data_train, label_train), (data_test, label_test) = load_data(dataset_str, opts)
+					data_train, data_test = preprocess_data_phase(opts, data_train, data_test)
+					data_gen = data_augmentation_phase(opts)
+					# COLLECT CALLBACKS
+					callback_list = collect_callbacks(opts)
 
-                    # TRAIN
-                    samples_per_epoch = data_train.shape[0] if opts['training_opts']['samples_per_epoch'] == -1 else opts['training_opts'][
-                        'samples_per_epoch']
-                    model.fit_generator(
-                        data_gen.flow(data_train, label_train, batch_size=opts['training_opts']['batch_size'], shuffle=True, seed=opts['seed']),
-                        samples_per_epoch=samples_per_epoch, nb_epoch=opts['training_opts']['epoch_nb'], callbacks=callback_list,
-                        validation_data=(data_test, label_test))
-                except:
-                    print(model_str, dataset_str)
-                    traceback.print_exc()
+					# TRAIN
+					samples_per_epoch = data_train.shape[0] if opts['training_opts']['samples_per_epoch'] == -1 else opts['training_opts'][
+						'samples_per_epoch']
+					model.fit_generator(
+						data_gen.flow(data_train, label_train, batch_size=opts['training_opts']['batch_size'], shuffle=True, seed=opts['seed']),
+						samples_per_epoch=samples_per_epoch, nb_epoch=opts['training_opts']['epoch_nb'], callbacks=callback_list,
+						validation_data=(data_test, label_test))
+				except:
+					print(model_str, dataset_str)
+					traceback.print_exc()
--- utils/trainingutils/training_phases_utils.py
+++ utils/trainingutils/training_phases_utils.py
@@ -17,92 +17,92 @@ cpu_debug = False
 
 
 def preprocess_data_phase(opts, data_train, data_test):
-    dataset_name = opt_utils.get_dataset_name(opts)
-    aug_opts = opt_utils.get_aug_opts(opts)
-    file_name_extension_format = '{}_zca:{}_stdn:{}_mn:{}.npy'
-    file_name_extension = file_name_extension_format.format(dataset_name, aug_opts['zca_whitening'], aug_opts['featurewise_std_normalization'], aug_opts['featurewise_center'])
-    train_name = 'xtrain_' + file_name_extension
-    test_name = 'xtest_' + file_name_extension
-    train_file_path = '/home/student/Documents/Codes/Python/DeepEperiments/PreprocessData/' + train_name
-    test_file_path = '/home/student/Documents/Codes/Python/DeepEperiments/PreprocessData/' + test_name
-    if aug_opts['enable']:
-        if not os.path.exists(os.path.abspath(train_file_path)):
-            print('Using real-time data augmentation.')
-            datagen = ImageDataGenerator(featurewise_center=aug_opts['featurewise_center'], featurewise_std_normalization=aug_opts['featurewise_std_normalization'],
-                                         zca_whitening=aug_opts['zca_whitening'])  # apply ZCA whitening)
-            print('Data Augmentation enabled')
-            print(aug_opts)
-            datagen.fit(data_train)
-            for i in range(data_train.shape[0]):
-                data_train[i] = datagen.standardize(data_train[i])
-            for i in range(data_test.shape[0]):
-                data_test[i] = datagen.standardize(data_test[i])
-            np.save(os.path.abspath(train_file_path), data_train)
-            np.save(os.path.abspath(test_file_path), data_test)
-        else:
-            data_train = np.load(os.path.abspath(train_file_path))
-            data_test = np.load(os.path.abspath(test_file_path))
-    return data_train, data_test
+	dataset_name = opt_utils.get_dataset_name(opts)
+	aug_opts = opt_utils.get_aug_opts(opts)
+	file_name_extension_format = '{}_zca:{}_stdn:{}_mn:{}.npy'
+	file_name_extension = file_name_extension_format.format(dataset_name, aug_opts['zca_whitening'], aug_opts['featurewise_std_normalization'], aug_opts['featurewise_center'])
+	train_name = 'xtrain_' + file_name_extension
+	test_name = 'xtest_' + file_name_extension
+	train_file_path = '/home/student/Documents/Git/DeepExperiments/PreprocessData/' + train_name
+	test_file_path = '/home/student/Documents/Git/DeepExperiments/PreprocessData/' + test_name
+	if aug_opts['enable']:
+		if not os.path.exists(os.path.abspath(train_file_path)):
+			print('Using real-time data augmentation.')
+			datagen = ImageDataGenerator(featurewise_center=aug_opts['featurewise_center'], featurewise_std_normalization=aug_opts['featurewise_std_normalization'],
+			                             zca_whitening=aug_opts['zca_whitening'])  # apply ZCA whitening)
+			print('Data Augmentation enabled')
+			print(aug_opts)
+			datagen.fit(data_train)
+			for i in range(data_train.shape[0]):
+				data_train[i] = datagen.standardize(data_train[i])
+			for i in range(data_test.shape[0]):
+				data_test[i] = datagen.standardize(data_test[i])
+			np.save(os.path.abspath(train_file_path), data_train)
+			np.save(os.path.abspath(test_file_path), data_test)
+		else:
+			data_train = np.load(os.path.abspath(train_file_path))
+			data_test = np.load(os.path.abspath(test_file_path))
+	return data_train, data_test
 
 
 def collect_callbacks(opts):
-    callback_list = []
-    result_manager = PlotMetrics(opts)
-    experiments_abs_path = result_manager.history_holder.dir_abs_path
-    callback_list += [result_manager]
+	callback_list = []
+	result_manager = PlotMetrics(opts)
+	experiments_abs_path = result_manager.history_holder.dir_abs_path
+	callback_list += [result_manager]
 
-    # callback_list += [ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=10, min_lr=0.0001)]
-    callback_list += [TensorboardVisualizer(log_dir=experiments_abs_path + '/logs', histogram_freq=1, write_graph=True, write_images=False)]
-    callback_list += [CSVLogger(filename=experiments_abs_path + '/training.log', separator=',')]
-    # callback_list += [EarlyStopping('acc', min_delta=.001, patience=20, mode='max')]
-    callback_list += [ModelCheckpoint(result_manager.history_holder.dir_abs_path + '/checkpoint', period=2,save_best_only=True,save_weights_only=True)]
-    callback_list += [LearningRateScheduler(lr_sched_fun_db.lr_sched_function_load(opt_utils.get_dataset_name(opts), opt_utils.get_lr_sched_family(opts)))]
-    callback_list += [TerminateOnNaN()]
-    return callback_list
+	# callback_list += [ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=10, min_lr=0.0001)]
+	callback_list += [TensorboardVisualizer(log_dir=experiments_abs_path + '/logs', histogram_freq=1, write_graph=True, write_images=False)]
+	callback_list += [CSVLogger(filename=experiments_abs_path + '/training.log', separator=',')]
+	# callback_list += [EarlyStopping('acc', min_delta=.001, patience=20, mode='max')]
+	callback_list += [ModelCheckpoint(result_manager.history_holder.dir_abs_path + '/checkpoint', period=2,save_best_only=True,save_weights_only=True)]
+	callback_list += [LearningRateScheduler(lr_sched_fun_db.lr_sched_function_load(opt_utils.get_dataset_name(opts), opt_utils.get_lr_sched_family(opts)))]
+	callback_list += [TerminateOnNaN()]
+	return callback_list
 
 
 def data_augmentation_phase(opts):
-    datagen = ImageDataGenerator()
-    if opts['aug_opts']['enable']:
-        print('Using real-time data augmentation.')
-        datagen = ImageDataGenerator(  # set input mean to 0 over the dataset
-            samplewise_center=opts['aug_opts']['samplewise_center'],  # set each sample mean to 0
-            samplewise_std_normalization=opts['aug_opts']['samplewise_std_normalization'],  # divide each input by its std
-            rotation_range=opts['aug_opts']['rotation_range'],  # randomly rotate images in the range (degrees, 0 to 180)
-            width_shift_range=opts['aug_opts']['width_shift_range'],  # randomly shift images horizontally (fraction of total width)
-            height_shift_range=opts['aug_opts']['height_shift_range'],  # randomly shift images vertically (fraction of total height)
-            horizontal_flip=opts['aug_opts']['horizontal_flip'],  # randomly flip images
-            vertical_flip=opts['aug_opts']['vertical_flip'])  # randomly flip images
-        print('Data Augmentation enabled')
-        print(opts['aug_opts'])
-    return datagen
+	datagen = ImageDataGenerator()
+	if opts['aug_opts']['enable']:
+		print('Using real-time data augmentation.')
+		datagen = ImageDataGenerator(  # set input mean to 0 over the dataset
+			samplewise_center=opts['aug_opts']['samplewise_center'],  # set each sample mean to 0
+			samplewise_std_normalization=opts['aug_opts']['samplewise_std_normalization'],  # divide each input by its std
+			rotation_range=opts['aug_opts']['rotation_range'],  # randomly rotate images in the range (degrees, 0 to 180)
+			width_shift_range=opts['aug_opts']['width_shift_range'],  # randomly shift images horizontally (fraction of total width)
+			height_shift_range=opts['aug_opts']['height_shift_range'],  # randomly shift images vertically (fraction of total height)
+			horizontal_flip=opts['aug_opts']['horizontal_flip'],  # randomly flip images
+			vertical_flip=opts['aug_opts']['vertical_flip'])  # randomly flip images
+		print('Data Augmentation enabled')
+		print(opts['aug_opts'])
+	return datagen
 
 
 def load_data(dataset, opts):
-    if dataset == 'cifar100':
-        (data_train, label_train), (data_test, label_test) = cifar100.load_data()
-    elif dataset == 'cifar10':
-        (data_train, label_train), (data_test, label_test) = cifar10.load_data()
-    else:
-        raise ValueError('dataset loader not defined')
-    nb_classes = opts['training_opts']['dataset']['nb_classes'];
-    label_train = np_utils.to_categorical(label_train, nb_classes)
-    label_test = np_utils.to_categorical(label_test, nb_classes)
-    data_train = data_train.astype('float32') / 255
-    data_test = data_test.astype('float32') / 255
-    return (data_train, label_train), (data_test, label_test)
+	if dataset == 'cifar100':
+		(data_train, label_train), (data_test, label_test) = cifar100.load_data()
+	elif dataset == 'cifar10':
+		(data_train, label_train), (data_test, label_test) = cifar10.load_data()
+	else:
+		raise ValueError('dataset loader not defined')
+	nb_classes = opts['training_opts']['dataset']['nb_classes'];
+	label_train = np_utils.to_categorical(label_train, nb_classes)
+	label_test = np_utils.to_categorical(label_test, nb_classes)
+	data_train = data_train.astype('float32') / 255
+	data_test = data_test.astype('float32') / 255
+	return (data_train, label_train), (data_test, label_test)
 
 
 def dataset_stats(generator):
-    sum = 0
-    mean = 0
-    for batch_index in np.arange(0, generator.nb_sample / generator.batch_size):
-        sum += np.mean(generator.next()[0], axis=(0, 1, 2))
-        if np.log2(batch_index + 1) % 1 == 0:
-            sum = sum / (float(batch_index + 1) / 2)
-            mean = (sum + mean) / 2
-            sum = 0
-    return mean
+	sum = 0
+	mean = 0
+	for batch_index in np.arange(0, generator.nb_sample / generator.batch_size):
+		sum += np.mean(generator.next()[0], axis=(0, 1, 2))
+		if np.log2(batch_index + 1) % 1 == 0:
+			sum = sum / (float(batch_index + 1) / 2)
+			mean = (sum + mean) / 2
+			sum = 0
+	return mean
 
 
 # def train(model, dataset_abs_path, optimizer, opts, cpu_debug=False):
