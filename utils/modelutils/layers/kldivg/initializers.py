fd0fb35f0ee44383c4bf61a6aff6bae2ef111ced
amir <a_emad@knights.ucf.edu>
2018-02-18 19:45:30 -0500

    Initializers added
    Log softmax init is set to uniform distribtuion on simplex, which is the dirichlet distribution with all parameters set to 1. 
    binary kl conv init is set to inverse of sigmoid

---
+++ utils/modelutils/layers/kldivg/initializers.py
@@ -0,0 +1,25 @@
+from keras.initializers import *
+import keras.backend as K
+class Sigmoid_Init(Initializer):
+    """Initializer that generates tensors uniform on log odds.
+    """
+
+    def __call__(self, shape, dtype=None):
+        out = K.random_uniform(shape=shape,
+                               minval=K.epsilon(),
+                               maxval=1-K.epsilon(),
+                               dtype=dtype)
+        out = -K.log((1/out)-1)
+        return out
+
+class Softmax_Init(Initializer):
+    """Initializer that generates tensors Uniform on simplex.
+    """
+
+    def __call__(self, shape, dtype=None):
+        out = K.random_uniform(shape=shape,
+                               minval=K.epsilon(),
+                               maxval=1-K.epsilon(),
+                               dtype=dtype)
+        out = K.log(out/K.sum(out, axis=2, keepdims=True))
+        return out
